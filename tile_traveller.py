5e1c992296474bb6a733d30d4ca2d0f2326f930b
Your NameDamian <damian18@ru.is>
2019-09-17 09:39:09 +0000

    tile_traveller.py

---
+++ tile_traveller.py
@@ -0,0 +1,6 @@
+#Define the grid 3*3
+#Starts in (1,1)
+#Displays the directions for which there are adjacent tiles that the player can travel to in each round
+#Enters the first letter for the direction player wants to go to. Capitalized or small 
+#If the player enters invalid direction, print "Not a valid direction!" and allows the player to enter the direction again
+#Tile (3,1) is the victory location. When entered progra notifies player of their victory and quits running
